fd71a11089794909fa248c830495eb14b0ca72a4
DanielJAch <danieljach@gmail.com>
2019-07-11 09:28:14 -0700

    More cleanup

---
+++ DotNETVueJSTemplate.Web/ClientApp/build/webpack.base.config.js
@@ -0,0 +1,138 @@
+﻿const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = (env) => {
+  const minimizeCss = env === 'production';
+  const chunkhash = env === 'development' ? '' : '.[chunkhash:7]';
+  const hash = env === 'development' ? '' : '.[hash:7]';
+  const bundleOutputDir = '../../dist';
+  const cssFileName = `site${chunkhash}.css`;
+
+  function resolve(dir) {
+    return path.join(__dirname, '..', dir);
+  }
+
+  function assetsPath(_path) {
+    const assetsSubDirectory = 'static';
+
+    return path.posix.join(assetsSubDirectory, _path);
+  }
+
+  return {
+    stats: { modules: false },
+    mode: env,
+
+    entry: {
+      'main': [
+        resolve('/src/app.js'),
+        resolve('../node_modules/bootstrap-vue/dist/bootstrap-vue.js'),
+        resolve('/src/styles/core.scss')
+      ]
+    },
+
+    output: {
+      path: path.join(__dirname, bundleOutputDir),
+      filename: `[name]${chunkhash}.js`,
+      publicPath: '/dist/'
+    },
+
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        'vue$': 'vue/dist/vue.js',
+        '@': resolve('/src', true)
+      }
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader!indentedSyntax'
+          }
+        },
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+          options: {
+            // eslint options (if necessary)
+            formatter: require('eslint-friendly-formatter')
+          }
+        },
+        {
+          test: /\.(scss|sass|css)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                minimize: minimizeCss,
+                sourceMap: true
+              }
+            },
+            'css-loader',
+            'sass-loader'
+          ]
+          // use: extractCss.extract([
+          //     {
+          //       loader: 'css-loader',
+          //       options: {
+          //         // minimize: minimizeCss,
+          //         sourceMap: true
+          //       }
+          //     },
+          //     {
+          //       loader: 'sass-loader',
+          //       options: {
+          //         // minimize: minimizeCss,
+          //         sourceMap: true
+          //       }
+          //     }
+          //   ])
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+            limit: 3000,
+            name: assetsPath(`img/[name]${hash}.[ext]`)
+          }
+        },
+        {
+          test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: assetsPath(`media/[name]${hash}.[ext]`)
+          }
+        },
+        {
+          test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: assetsPath(`fonts/[name]${hash}.[ext]`)
+          }
+        }
+      ]
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: cssFileName
+      }),
+      new VueLoaderPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: ''
+      })
+    ]
+  };
+};
---
+++ DotNETVueJSTemplate.Web/ClientApp/build/webpack.dev.config.js
@@ -0,0 +1,18 @@
+﻿const webpack = require('webpack');
+const merge = require('webpack-merge');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+const config = require('../config');
+const baseWebpackConfig = require('./webpack.base.config');
+
+module.exports = merge(baseWebpackConfig('development'), {
+  devtool: '#inline-source-map', // inline
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env,
+      '_config.urls': config.dev.urls
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new FriendlyErrorsPlugin()
+  ]
+});
